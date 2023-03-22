5e18896f7c2f3aceb283f9a427bd61893a3e0f06
Arthur Leonardo Filipus <ArthurFilipus@hotmail.com>
2023-03-22 16:50:41 -0300

    código refatorado

--- RoboTupiniquim/Program.cs
+++ RoboTupiniquim/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RoboTupiniquim
 {
@@ -6,180 +7,116 @@ namespace RoboTupiniquim
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite o tamanho do grid X: ");
+            for (int i = 0; i < 2; i++)
+            {
+                PosicaoInicialRobo();
 
-            int gridx = Convert.ToInt32(Console.ReadLine());
+                MovimentacaoRobo();  
+            }
+        }
 
-            Console.Write("Digite o tamanho do grid Y: ");
+        #region Variáveis Globais
+        static int posinicialx;
+        static int posinicialy;
+        static char direcao;
+        static string comando;
+        static char[] comandochar;
+        static int robo;
+        static int nomerobo;
+        #endregion
+
+        public static void PosicaoInicialRobo()
+        {
+            nomerobo++;
             
-            int gridy = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Robo1");
+            Console.WriteLine($"Robo{nomerobo}");
 
             Console.Write("Digite a posição inicial X: ");
 
-            int posinicialx1 = Convert.ToInt32(Console.ReadLine());
+            posinicialx = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Digite a posição inicial Y: ");
 
-            int posinicialy1 = Convert.ToInt32(Console.ReadLine());
+            posinicialy = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Insira a direção do Robo em (N) (S) (L) (O): ");
 
-            char direcao1 = Convert.ToChar(Console.ReadLine());
+            direcao = Convert.ToChar(Console.ReadLine().ToUpper());
 
             Console.Write("Digite o comando para o robo fazer: ");
 
-            string comando1 = Console.ReadLine();
+            comando = Console.ReadLine().ToUpper();
 
-            char[] comandochar1 = comando1.ToCharArray();
+            comandochar = comando.ToCharArray();
+        }
 
-            for (int i = 0; i < comandochar1.Length; i++)
+        public static void MovimentacaoRobo()
+        {
+            for (int i = 0; i < comandochar.Length; i++)
             {
-                if (comandochar1[i] == 'M')
+                if (comandochar[i] == 'M')
                 {
-                    if (direcao1 == 'N')
+                    if (direcao == 'N')
                     {
-                        posinicialy1++;
+                        posinicialy++;
                     }
-                    else if (direcao1 == 'S')
+                    else if (direcao == 'S')
                     {
-                        posinicialy1--;
+                        posinicialy--;
                     }
-                    else if (direcao1 == 'L')
+                    else if (direcao == 'L')
                     {
-                        posinicialx1++;
+                        posinicialx++;
                     }
-                    else if (direcao1 == 'O')
+                    else if (direcao == 'O')
                     {
-                        posinicialx1--;
+                        posinicialx--;
                     }
                 }
-                else if (comandochar1[i] == 'E')
+                else if (comandochar[i] == 'E')
                 {
-                    if (direcao1 == 'N')
+                    if (direcao == 'N')
                     {
-                        direcao1 = 'O';
+                        direcao = 'O';
                     }
-                    else if (direcao1 == 'O')
+                    else if (direcao == 'O')
                     {
-                        direcao1 = 'S';
+                        direcao = 'S';
                     }
-                    else if (direcao1 == 'S')
+                    else if (direcao == 'S')
                     {
-                        direcao1 = 'L';
+                        direcao = 'L';
                     }
-                    else if (direcao1 == 'L')
+                    else if (direcao == 'L')
                     {
-                        direcao1 = 'N';
+                        direcao = 'N';
                     }
                 }
-                else if (comandochar1[i] == 'D')
+                else if (comandochar[i] == 'D')
                 {
-                    if (direcao1 == 'N')
+                    if (direcao == 'N')
                     {
-                        direcao1 = 'L';
+                        direcao = 'L';
                     }
-                    else if (direcao1 == 'L')
+                    else if (direcao == 'L')
                     {
-                        direcao1 = 'S';
+                        direcao = 'S';
                     }
-                    else if (direcao1 == 'S')
+                    else if (direcao == 'S')
                     {
-                        direcao1 = 'O';
+                        direcao = 'O';
                     }
-                    else if (direcao1 == 'O')
+                    else if (direcao == 'O')
                     {
-                        direcao1 = 'N';
+                        direcao = 'N';
                     }
-                }
+                }                
             }
+            robo++;
 
-            Console.WriteLine("Robo2");
+            Console.WriteLine($"Robo{robo}: {posinicialx} , {posinicialy} {direcao}");
 
-            Console.Write("Digite a posição inicial X: ");
-
-            int posinicialx2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Digite a posição inicial Y: ");
-
-            int posinicialy2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Insira a direção do Robo em (N) (S) (L) (O): ");
-
-            char direcao2 = Convert.ToChar(Console.ReadLine());
-
-            Console.Write("Digite o comando para o robo fazer: ");
-
-            string comando2 = Console.ReadLine();
-
-            char[] comandochar2 = comando2.ToCharArray();
-
-            for (int i = 0; i < comandochar2.Length; i++)
-            {
-                if (comandochar2[i] == 'M')
-                {
-                    if (direcao2 == 'N')
-                    {
-                        posinicialy2++;
-                    }
-                    else if (direcao2 == 'S')
-                    {
-                        posinicialy2--;
-                    }
-                    else if (direcao2 == 'L')
-                    {
-                        posinicialx2++;
-                    }
-                    else if (direcao2 == 'O')
-                    {
-                        posinicialx2--;
-                    }
-                }
-                else if (comandochar2[i] == 'E')
-                {
-                    if (direcao2 == 'N')
-                    {
-                        direcao2 = 'O';
-                    }
-                    else if (direcao2 == 'O')
-                    {
-                        direcao2 = 'S';
-                    }
-                    else if (direcao2 == 'S')
-                    {
-                        direcao2 = 'L';
-                    }
-                    else if (direcao2 == 'L')
-                    {
-                        direcao2 = 'N';
-                    }
-                }
-                else if (comandochar2[i] == 'D')
-                {
-                    if (direcao2 == 'N')
-                    {
-                        direcao2 = 'L';
-                    }
-                    else if (direcao2 == 'L')
-                    {
-                        direcao2 = 'S';
-                    }
-                    else if (direcao2 == 'S')
-                    {
-                        direcao2 = 'O';
-                    }
-                    else if (direcao2 == 'O')
-                    {
-                        direcao2 = 'N';
-                    }
-                }
-            }
-            Console.WriteLine($"Robo1: {posinicialx1} , {posinicialy1} {direcao1}");
-
-            Console.WriteLine($"Robo2: {posinicialx2} , {posinicialy2} {direcao2}");
-
-            Console.ReadLine();
+            Console.WriteLine();
         }
     }
 }
